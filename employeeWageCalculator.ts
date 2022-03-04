506d8c272554b450bc2ed09d8c86088b22622cb1
Grid112 <sumitware1999@gmail.com>
2022-03-05 00:16:03 +0530

    Employee Attendance

--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,1 +1,9 @@
 console.log("Welcome to Employee Wage Calculator");
+var IS_ABSENT = 0;
+var empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == 0) {
+    console.log("EMPLOYEE ABSENT");
+}
+else {
+    console.log("EMPLOYEE PRESENT");
+}
