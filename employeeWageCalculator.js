4c2cfb3a2518b0ff6cf4a8da292f188eb157eb8c
Grid112 <sumitware1999@gmail.com>
2022-03-05 00:41:05 +0530

    Employee Part Time Wage

--- employeeWageCalculator.js
+++ employeeWageCalculator.js
@@ -1,16 +1,25 @@
 console.log("Welcome to Employee Wage Calculator");
+var Is_Present = 1;
 var Is_Full_Time = 1;
 var Emp_Rate_Per_Hr = 20;
 var Emp_Work_Hrs = 0;
 var Emp_Wage = 0;
 var empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 1) {
+var empDailyCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == Is_Present) {
     console.log("Employee Is Present");
-    Emp_Work_Hrs = 8;
+    if (empDailyCheck == Is_Full_Time) {
+        console.log("Employee Is Present For FULL Day");
+        Emp_Work_Hrs = 8;
+    }
+    else {
+        console.log("Employee Is Present For HALF Day");
+        Emp_Work_Hrs = 4;
+    }
 }
 else {
     console.log("Employee Is Absent");
     Emp_Work_Hrs = 0;
 }
 Emp_Wage = Emp_Work_Hrs * Emp_Rate_Per_Hr;
-console.log("Employee Daily Wage IS", Emp_Wage);
+console.log("Employee Daily Wage Is", Emp_Wage);
--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,14 +1,22 @@
 console.log("Welcome to Employee Wage Calculator");
-var Is_Full_Time = 1;
-var Emp_Rate_Per_Hr = 20;
-var Emp_Work_Hrs  = 0;
-var Emp_Wage = 0;
+var Is_Present : number = 1;
+var Is_Full_Time : number = 1;
+var Emp_Rate_Per_Hr : number = 20;
+var Emp_Work_Hrs : number = 0;
+var Emp_Wage : number = 0;
 
 var empCheck = Math.floor(Math.random() * 10) % 2;
+var empDailyCheck = Math.floor(Math.random() * 10) % 2;
 
-if (empCheck == 1) {
+if (empCheck == Is_Present) {
     console.log("Employee Is Present");
-    Emp_Work_Hrs  = 8;
+    if (empDailyCheck == Is_Full_Time) {
+        console.log("Employee Is Present For FULL Day");
+        Emp_Work_Hrs  = 8;    
+    } else {
+        console.log("Employee Is Present For HALF Day");
+        Emp_Work_Hrs  = 4;    
+    }
 }
 else {
     console.log("Employee Is Absent");
