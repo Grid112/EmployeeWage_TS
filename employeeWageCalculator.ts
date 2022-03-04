9c6ec38a02921e8d92d122bb507d00f871b84b2b
Grid112 <sumitware1999@gmail.com>
2022-03-05 00:29:20 +0530

    Employee Daily Wage

--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,9 +1,19 @@
 console.log("Welcome to Employee Wage Calculator");
-var IS_ABSENT = 0;
+var Is_Full_Time = 1;
+var Emp_Rate_Per_Hr = 20;
+var Emp_Work_Hrs  = 0;
+var Emp_Wage = 0;
+
 var empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == 0) {
-    console.log("EMPLOYEE ABSENT");
+
+if (empCheck == 1) {
+    console.log("Employee Is Present");
+    Emp_Work_Hrs  = 8;
 }
 else {
-    console.log("EMPLOYEE PRESENT");
+    console.log("Employee Is Absent");
+    Emp_Work_Hrs  = 0;
 }
+
+Emp_Wage = Emp_Work_Hrs * Emp_Rate_Per_Hr;
+console.log("Employee Daily Wage Is", Emp_Wage)
