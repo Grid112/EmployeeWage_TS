62ec2d9c318fc8ebbce493378fda028d2af77506
Grid112 <sumitware1999@gmail.com>
2022-03-05 00:48:38 +0530

    Employee Wage Using Switch Case

--- employeeWageCalculator.js
+++ employeeWageCalculator.js
@@ -4,22 +4,44 @@ var Is_Full_Time = 1;
 var Emp_Rate_Per_Hr = 20;
 var Emp_Work_Hrs = 0;
 var Emp_Wage = 0;
-var empCheck = Math.floor(Math.random() * 10) % 2;
-var empDailyCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == Is_Present) {
-    console.log("Employee Is Present");
-    if (empDailyCheck == Is_Full_Time) {
+var empCheck = Math.floor(Math.random() * 10) % 3;
+// var empDailyCheck = Math.floor(Math.random() * 10) % 2;
+switch (empCheck) {
+    case 0: {
+        console.log("EMPLOYEE ABSENT");
+        Emp_Work_Hrs = 0;
+        break;
+    }
+    case 1: {
         console.log("Employee Is Present For FULL Day");
         Emp_Work_Hrs = 8;
+        break;
     }
-    else {
+    case 2: {
         console.log("Employee Is Present For HALF Day");
         Emp_Work_Hrs = 4;
+        break;
     }
+    default:
+        {
+            console.log("Employee Not Found");
+            break;
+        }
+        ;
 }
-else {
-    console.log("Employee Is Absent");
-    Emp_Work_Hrs = 0;
-}
+// if (empCheck == Is_Present) {
+//     console.log("Employee Is Present");
+//     if (empDailyCheck == Is_Full_Time) {
+//         console.log("Employee Is Present For FULL Day");
+//         Emp_Work_Hrs = 8;
+//     } else {
+//         console.log("Employee Is Present For HALF Day");
+//         Emp_Work_Hrs = 4;
+//     }
+// }
+// else {
+//     console.log("Employee Is Absent");
+//     Emp_Work_Hrs = 0;
+// }
 Emp_Wage = Emp_Work_Hrs * Emp_Rate_Per_Hr;
 console.log("Employee Daily Wage Is", Emp_Wage);
--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,26 +1,31 @@
 console.log("Welcome to Employee Wage Calculator");
-var Is_Present : number = 1;
-var Is_Full_Time : number = 1;
-var Emp_Rate_Per_Hr : number = 20;
-var Emp_Work_Hrs : number = 0;
-var Emp_Wage : number = 0;
+var Is_Present: number = 1;
+var Emp_Rate_Per_Hr: number = 20;
+var Emp_Work_Hrs: number = 0;
+var Emp_Wage: number = 0;
 
-var empCheck = Math.floor(Math.random() * 10) % 2;
-var empDailyCheck = Math.floor(Math.random() * 10) % 2;
+var empCheck = Math.floor(Math.random() * 10) % 3;
 
-if (empCheck == Is_Present) {
-    console.log("Employee Is Present");
-    if (empDailyCheck == Is_Full_Time) {
-        console.log("Employee Is Present For FULL Day");
-        Emp_Work_Hrs  = 8;    
-    } else {
-        console.log("Employee Is Present For HALF Day");
-        Emp_Work_Hrs  = 4;    
+switch (empCheck) {
+    case 0:{
+        console.log("EMPLOYEE ABSENT");
+        Emp_Work_Hrs = 0;
+        break;
     }
-}
-else {
-    console.log("Employee Is Absent");
-    Emp_Work_Hrs  = 0;
+    case 1:{
+        console.log("Employee Is Present For FULL Day");
+        Emp_Work_Hrs = 8;    
+        break;
+    }
+    case 2:{
+        console.log("Employee Is Present For HALF Day");
+        Emp_Work_Hrs = 4;
+        break;
+    }
+    default:{
+        console.log("Employee Not Found");
+        break;
+    };
 }
 
 Emp_Wage = Emp_Work_Hrs * Emp_Rate_Per_Hr;
