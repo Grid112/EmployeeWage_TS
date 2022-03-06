ca3b9855750a32746bb1c513d8bcd01818c8e8dc
Grid112 <sumitware1999@gmail.com>
2022-03-06 21:49:48 +0530

    Calculating Employee Monthly Wages

--- employeeWageCalculator.js
+++ employeeWageCalculator.js
@@ -1,47 +1,38 @@
-console.log("Welcome to Employee Wage Calculator");
-var Is_Present = 1;
-var Is_Full_Time = 1;
+console.log("\nWelcome to Employee Wage Calculator \n");
 var Emp_Rate_Per_Hr = 20;
+var Working_Days_Per_Month = 0;
 var Emp_Work_Hrs = 0;
-var Emp_Wage = 0;
-var empCheck = Math.floor(Math.random() * 10) % 3;
-// var empDailyCheck = Math.floor(Math.random() * 10) % 2;
-switch (empCheck) {
-    case 0: {
-        console.log("EMPLOYEE ABSENT");
-        Emp_Work_Hrs = 0;
-        break;
-    }
-    case 1: {
-        console.log("Employee Is Present For FULL Day");
-        Emp_Work_Hrs = 8;
-        break;
-    }
-    case 2: {
-        console.log("Employee Is Present For HALF Day");
-        Emp_Work_Hrs = 4;
-        break;
-    }
-    default:
-        {
-            console.log("Employee Not Found");
+var Total_Emp_Work_Hrs = 0;
+var Total_Emp_Wage = 0;
+var Emp_Working_Days = 0;
+for (Working_Days_Per_Month = 0; Working_Days_Per_Month < 20; Working_Days_Per_Month++) {
+    var empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+        case 0: {
+            console.log("Employee Is Absent");
+            Emp_Work_Hrs = 0;
             break;
         }
-        ;
+        case 1: {
+            console.log("Employee Is Present For FULL Day");
+            Emp_Work_Hrs = 8;
+            break;
+        }
+        case 2: {
+            console.log("Employee Is Present For HALF Day");
+            Emp_Work_Hrs = 4;
+            break;
+        }
+        default:
+            {
+                console.log("Employee Not Found");
+                break;
+            }
+            ;
+    }
+    Total_Emp_Work_Hrs += Emp_Work_Hrs;
+    console.log("Employee Working Days Are: ", Emp_Working_Days);
+    Emp_Working_Days++;
 }
-// if (empCheck == Is_Present) {
-//     console.log("Employee Is Present");
-//     if (empDailyCheck == Is_Full_Time) {
-//         console.log("Employee Is Present For FULL Day");
-//         Emp_Work_Hrs = 8;
-//     } else {
-//         console.log("Employee Is Present For HALF Day");
-//         Emp_Work_Hrs = 4;
-//     }
-// }
-// else {
-//     console.log("Employee Is Absent");
-//     Emp_Work_Hrs = 0;
-// }
-Emp_Wage = Emp_Work_Hrs * Emp_Rate_Per_Hr;
-console.log("Employee Daily Wage Is", Emp_Wage);
+Total_Emp_Wage = Emp_Rate_Per_Hr * Total_Emp_Work_Hrs;
+console.log("\nTotal Employee Wage is :", Total_Emp_Wage, "\n");
--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,32 +1,42 @@
-console.log("Welcome to Employee Wage Calculator");
-var Is_Present: number = 1;
-var Emp_Rate_Per_Hr: number = 20;
+console.log("\nWelcome to Employee Wage Calculator \n");
+
+const Emp_Rate_Per_Hr: number = 20;
+var Working_Days_Per_Month: number = 0;
 var Emp_Work_Hrs: number = 0;
-var Emp_Wage: number = 0;
+var Total_Emp_Work_Hrs: number = 0;
+var Total_Emp_Wage: number = 0;
+var Emp_Working_Days: number = 0;
 
-var empCheck = Math.floor(Math.random() * 10) % 3;
+for (Working_Days_Per_Month = 0; Working_Days_Per_Month < 20; Working_Days_Per_Month++) {
+    
+    var empCheck = Math.floor(Math.random() * 10) % 3;
 
-switch (empCheck) {
-    case 0:{
-        console.log("EMPLOYEE ABSENT");
-        Emp_Work_Hrs = 0;
-        break;
+    switch (empCheck) {
+        case 0: {
+            console.log("Employee Is Absent");
+            Emp_Work_Hrs = 0;
+            break;
+        }
+        case 1: {
+            console.log("Employee Is Present For FULL Day");
+            Emp_Work_Hrs = 8;
+            break;
+        }
+        case 2: {
+            console.log("Employee Is Present For HALF Day");
+            Emp_Work_Hrs = 4;
+            break;
+        }
+        default: {
+            console.log("Employee Not Found");
+            break;
+        };
     }
-    case 1:{
-        console.log("Employee Is Present For FULL Day");
-        Emp_Work_Hrs = 8;    
-        break;
-    }
-    case 2:{
-        console.log("Employee Is Present For HALF Day");
-        Emp_Work_Hrs = 4;
-        break;
-    }
-    default:{
-        console.log("Employee Not Found");
-        break;
-    };
+
+    Total_Emp_Work_Hrs += Emp_Work_Hrs;
+    console.log("Employee Working Days Are: ", Emp_Working_Days);
+    Emp_Working_Days++;    
 }
+Total_Emp_Wage = Emp_Rate_Per_Hr * Total_Emp_Work_Hrs;
+console.log("\nTotal Employee Wage In One Month Is :", Total_Emp_Wage , "\n");
 
-Emp_Wage = Emp_Work_Hrs * Emp_Rate_Per_Hr;
-console.log("Employee Daily Wage Is", Emp_Wage)
