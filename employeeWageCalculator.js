3dfa0b3684740526459c4629a13a367fac9098f3
Grid112 <sumitware1999@gmail.com>
2022-03-06 22:29:13 +0530

    Calculating Employee Wages Until Max Working Hrs Or Working days per Month Have Reached

--- employeeWageCalculator.js
+++ employeeWageCalculator.js
@@ -1,14 +1,16 @@
 console.log("\nWelcome to Employee Wage Calculator \n");
 var Emp_Rate_Per_Hr = 20;
-var Working_Days_Per_Month = 0;
+var Working_Days_Per_Month = 20;
 var Emp_Work_Hrs = 0;
 var Total_Emp_Work_Hrs = 0;
 var Total_Emp_Wage = 0;
 var Emp_Working_Days = 0;
-for (Working_Days_Per_Month = 0; Working_Days_Per_Month < 20; Working_Days_Per_Month++) {
+var Emp_Absent_Days = 0;
+while (Total_Emp_Work_Hrs <= 100 && Emp_Working_Days < 20) {
     var empCheck = Math.floor(Math.random() * 10) % 3;
     switch (empCheck) {
         case 0: {
+            Emp_Absent_Days++;
             console.log("Employee Is Absent");
             Emp_Work_Hrs = 0;
             break;
@@ -30,9 +32,11 @@ for (Working_Days_Per_Month = 0; Working_Days_Per_Month < 20; Working_Days_Per_M
             }
             ;
     }
+    Emp_Working_Days++;
     Total_Emp_Work_Hrs += Emp_Work_Hrs;
     console.log("Employee Working Days Are: ", Emp_Working_Days);
-    Emp_Working_Days++;
+    console.log("Employee Absent Days Are: ", Emp_Absent_Days, "\n");
 }
 Total_Emp_Wage = Emp_Rate_Per_Hr * Total_Emp_Work_Hrs;
-console.log("\nTotal Employee Wage is :", Total_Emp_Wage, "\n");
+console.log("\nTotal Employee Workinh Hours In A Month: ", Total_Emp_Work_Hrs);
+console.log("total Employee Wage In One Month Is :", Total_Emp_Wage, "\n");
--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,29 +1,35 @@
 console.log("\nWelcome to Employee Wage Calculator \n");
 
 const Emp_Rate_Per_Hr: number = 20;
-var Working_Days_Per_Month: number = 0;
+const Working_Days_Per_Month: number = 20;
 var Emp_Work_Hrs: number = 0;
 var Total_Emp_Work_Hrs: number = 0;
 var Total_Emp_Wage: number = 0;
 var Emp_Working_Days: number = 0;
+var Emp_Absent_Days: number = 0;
 
-for (Working_Days_Per_Month = 0; Working_Days_Per_Month < 20; Working_Days_Per_Month++) {
+while (Total_Emp_Work_Hrs <= 100 && Emp_Working_Days < 20) {
     
     var empCheck = Math.floor(Math.random() * 10) % 3;
 
     switch (empCheck) {
         case 0: {
+            Emp_Absent_Days++;
             console.log("Employee Is Absent");
             Emp_Work_Hrs = 0;
             break;
         }
         case 1: {
+            
             console.log("Employee Is Present For FULL Day");
+            
             Emp_Work_Hrs = 8;
             break;
         }
         case 2: {
+            
             console.log("Employee Is Present For HALF Day");
+
             Emp_Work_Hrs = 4;
             break;
         }
@@ -32,11 +38,13 @@ for (Working_Days_Per_Month = 0; Working_Days_Per_Month < 20; Working_Days_Per_M
             break;
         };
     }
+    Emp_Working_Days++;
 
     Total_Emp_Work_Hrs += Emp_Work_Hrs;
     console.log("Employee Working Days Are: ", Emp_Working_Days);
-    Emp_Working_Days++;    
+    console.log("Employee Absent Days Are: ", Emp_Absent_Days , "\n");
 }
 Total_Emp_Wage = Emp_Rate_Per_Hr * Total_Emp_Work_Hrs;
-console.log("\nTotal Employee Wage In One Month Is :", Total_Emp_Wage , "\n");
+console.log("\nTotal Employee Workinh Hours In A Month: ", Total_Emp_Work_Hrs);
+console.log("total Employee Wage In One Month Is :", Total_Emp_Wage, "\n");
 
