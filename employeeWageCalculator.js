4452519828a395b992b718ea20097430d88eefc0
Grid112 <sumitware1999@gmail.com>
2022-03-14 17:57:28 +0530

    Executing Emp Wage While Using Function And Classes

--- employeeWageCalculator.js
+++ employeeWageCalculator.js
@@ -1,42 +1,52 @@
 console.log("\nWelcome to Employee Wage Calculator \n");
-var Emp_Rate_Per_Hr = 20;
-var Working_Days_Per_Month = 20;
-var Emp_Work_Hrs = 0;
-var Total_Emp_Work_Hrs = 0;
-var Total_Emp_Wage = 0;
-var Emp_Working_Days = 0;
-var Emp_Absent_Days = 0;
-while (Total_Emp_Work_Hrs < 100 && Emp_Working_Days < 20) {
-    var empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
-    switch (empAttendanceCheck) {
-        case 0: {
-            Emp_Absent_Days++;
-            console.log("Employee Is Absent");
-            Emp_Work_Hrs = 0;
-            break;
-        }
-        case 1: {
-            console.log("Employee Is Present For FULL Day");
-            Emp_Work_Hrs = 8;
-            break;
-        }
-        case 2: {
-            console.log("Employee Is Present For HALF Day");
-            Emp_Work_Hrs = 4;
-            break;
-        }
-        default:
-            {
-                console.log("Employee Not Found");
-                break;
-            }
-            ;
+var EmployeeWage = /** @class */ (function () {
+    function EmployeeWage() {
+        this.Emp_Rate_Per_Hr = 20;
+        this.Working_Days_Per_Month = 20;
+        this.Emp_Work_Hrs = 0;
+        this.Total_Emp_Work_Hrs = 0;
+        this.Total_Emp_Wage = 0;
+        this.Emp_Working_Days = 0;
+        this.Emp_Absent_Days = 0;
     }
-    Emp_Working_Days++;
-    Total_Emp_Work_Hrs += Emp_Work_Hrs;
-    console.log("Employee Working Days Are: ", Emp_Working_Days);
-    console.log("Employee Absent Days Are: ", Emp_Absent_Days, "\n");
-}
-Total_Emp_Wage = Emp_Rate_Per_Hr * Total_Emp_Work_Hrs;
-console.log("\nTotal Employee Workinh Hours In A Month: ", Total_Emp_Work_Hrs);
-console.log("total Employee Wage In One Month Is :", Total_Emp_Wage, "\n");
+    EmployeeWage.prototype.empDailyCheck = function () {
+        while (this.Total_Emp_Work_Hrs < 100 && this.Emp_Working_Days < 20) {
+            var empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
+            switch (empAttendanceCheck) {
+                case 0: {
+                    this.Emp_Absent_Days++;
+                    console.log("Employee Is Absent");
+                    this.Emp_Work_Hrs = 0;
+                    break;
+                }
+                case 1: {
+                    console.log("Employee Is Present For FULL Day");
+                    this.Emp_Work_Hrs = 8;
+                    break;
+                }
+                case 2: {
+                    console.log("Employee Is Present For HALF Day");
+                    this.Emp_Work_Hrs = 4;
+                    break;
+                }
+                default:
+                    {
+                        console.log("Employee Not Found");
+                        break;
+                    }
+                    ;
+            }
+            this.Emp_Working_Days++;
+            this.Total_Emp_Work_Hrs += this.Emp_Work_Hrs;
+            console.log("Employee Working Days Are: ", this.Emp_Working_Days);
+            console.log("Employee Absent Days Are: ", this.Emp_Absent_Days, "\n");
+            this.Total_Emp_Wage = this.Emp_Rate_Per_Hr * this.Total_Emp_Work_Hrs;
+            console.log("\nTotal Employee Workinh Hours In A Month: ", this.Total_Emp_Work_Hrs);
+            console.log("total Employee Wage In One Month Is :", this.Total_Emp_Wage, "\n");
+            // return this.Total_Emp_Work_Hrs;
+        }
+    };
+    return EmployeeWage;
+}());
+var employeeWage = new EmployeeWage();
+employeeWage.empDailyCheck();
--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,47 +1,56 @@
 console.log("\nWelcome to Employee Wage Calculator \n");
+class EmployeeWage {
+     Emp_Rate_Per_Hr: number = 20;
+     Working_Days_Per_Month: number = 20;
+     Emp_Work_Hrs: number = 0;
+     Total_Emp_Work_Hrs: number = 0;
+     Total_Emp_Wage: number = 0;
+     Emp_Working_Days: number = 0;
+     Emp_Absent_Days: number = 0;
 
-const Emp_Rate_Per_Hr: number = 20;
-const Working_Days_Per_Month: number = 20;
-var Emp_Work_Hrs: number = 0;
-var Total_Emp_Work_Hrs: number = 0;
-var Total_Emp_Wage: number = 0;
-var Emp_Working_Days: number = 0;
-var Emp_Absent_Days: number = 0;
 
-while (Total_Emp_Work_Hrs < 100 && Emp_Working_Days < 20) {
+    empDailyCheck(): void {
+        while (this.Total_Emp_Work_Hrs < 100 && this.Emp_Working_Days < 20) {
 
-    var empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
+            var empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
 
-    switch (empAttendanceCheck) {
-        case 0: {
-            Emp_Absent_Days++;
-            console.log("Employee Is Absent");
-            Emp_Work_Hrs = 0;
-            break;
+            switch (empAttendanceCheck) {
+                case 0: {
+                    this.Emp_Absent_Days++;
+                    console.log("Employee Is Absent");
+                    this.Emp_Work_Hrs = 0;
+                    break;
+                }
+                case 1: {
+
+                    console.log("Employee Is Present For FULL Day");
+                    this.Emp_Work_Hrs = 8;
+                    break;
+                }
+                case 2: {
+                    console.log("Employee Is Present For HALF Day");
+                    this.Emp_Work_Hrs = 4;
+                    break;
+                }
+                default: {
+                    console.log("Employee Not Found");
+                    break;
+                };
+            }
+            this.Emp_Working_Days++;
+
+            this.Total_Emp_Work_Hrs += this.Emp_Work_Hrs;
+            console.log("Employee Working Days Are: ", this.Emp_Working_Days);
+            console.log("Employee Absent Days Are: ", this.Emp_Absent_Days, "\n");
+            this.Total_Emp_Wage = this.Emp_Rate_Per_Hr * this.Total_Emp_Work_Hrs;
+            console.log("\nTotal Employee Workinh Hours In A Month: ", this.Total_Emp_Work_Hrs);
+            console.log("total Employee Wage In One Month Is :", this.Total_Emp_Wage, "\n");
+            // return this.Total_Emp_Work_Hrs;
+
         }
-        case 1: {
-            
-            console.log("Employee Is Present For FULL Day");
-            Emp_Work_Hrs = 8;
-            break;
-        }
-        case 2: {
-            console.log("Employee Is Present For HALF Day");
-            Emp_Work_Hrs = 4;
-            break;
-        }
-        default: {
-            console.log("Employee Not Found");
-            break;
-        };
-    }
-    Emp_Working_Days++;
-
-    Total_Emp_Work_Hrs += Emp_Work_Hrs;
-    console.log("Employee Working Days Are: ", Emp_Working_Days);
-    console.log("Employee Absent Days Are: ", Emp_Absent_Days, "\n");
+    }    
 }
-Total_Emp_Wage = Emp_Rate_Per_Hr * Total_Emp_Work_Hrs;
-console.log("\nTotal Employee Workinh Hours In A Month: ", Total_Emp_Work_Hrs);
-console.log("total Employee Wage In One Month Is :", Total_Emp_Wage, "\n");
+var employeeWage = new EmployeeWage();
+employeeWage.empDailyCheck();
 
+    
