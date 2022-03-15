e892211ee82b71daf250b683341026eb06fbf448
Grid112 <sumitware1999@gmail.com>
2022-03-15 21:35:32 +0530

    Calculating Wages for Different Companies Seperately

--- employeeWageCalculator.js
+++ employeeWageCalculator.js
@@ -1,52 +1,49 @@
 console.log("\nWelcome to Employee Wage Calculator \n");
-var EmployeeWage = /** @class */ (function () {
-    function EmployeeWage() {
-        this.Emp_Rate_Per_Hr = 20;
-        this.Working_Days_Per_Month = 20;
-        this.Emp_Work_Hrs = 0;
-        this.Total_Emp_Work_Hrs = 0;
-        this.Total_Emp_Wage = 0;
-        this.Emp_Working_Days = 0;
-        this.Emp_Absent_Days = 0;
+var EmpWageBuilder = /** @class */ (function () {
+    function EmpWageBuilder(companyName, emp_ratePerHrs, workingDays, workingHrs) {
+        this.companyName = companyName;
+        this.empRatePerHrs = emp_ratePerHrs;
+        this.workingDays = workingDays;
+        this.workingHrs = workingHrs;
     }
-    EmployeeWage.prototype.empDailyCheck = function () {
-        while (this.Total_Emp_Work_Hrs < 100 && this.Emp_Working_Days < 20) {
-            var empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
-            switch (empAttendanceCheck) {
-                case 0: {
-                    this.Emp_Absent_Days++;
-                    console.log("Employee Is Absent");
-                    this.Emp_Work_Hrs = 0;
+    EmpWageBuilder.prototype.Function_EmpWage = function () {
+        var emphrs = 0;
+        var totalemphrs = 0;
+        var totalworkingday = 0;
+        var absentDay = 0;
+        while (totalemphrs <= this.workingHrs || totalworkingday < this.workingDays) {
+            var CheckEmp = Math.floor(Math.random() * 10 % 3);
+            switch (CheckEmp) {
+                case 1:
+                    emphrs = 8;
+                    console.log("Employee is full-time present----Days are : " + totalworkingday
+                        + " & Employee Hrs is : " + emphrs);
                     break;
-                }
-                case 1: {
-                    console.log("Employee Is Present For FULL Day");
-                    this.Emp_Work_Hrs = 8;
+                case 2:
+                    emphrs = 4;
+                    console.log("Employee is part-time present----Days are : " + totalworkingday
+                        + " & Employee Hrs is : " + emphrs);
                     break;
-                }
-                case 2: {
-                    console.log("Employee Is Present For HALF Day");
-                    this.Emp_Work_Hrs = 4;
-                    break;
-                }
                 default:
-                    {
-                        console.log("Employee Not Found");
-                        break;
-                    }
-                    ;
+                    console.log("Employee is absent----Day are : " + absentDay);
+                    emphrs = 0;
+                    break;
             }
-            this.Emp_Working_Days++;
-            this.Total_Emp_Work_Hrs += this.Emp_Work_Hrs;
-            console.log("Employee Working Days Are: ", this.Emp_Working_Days);
-            console.log("Employee Absent Days Are: ", this.Emp_Absent_Days, "\n");
-            this.Total_Emp_Wage = this.Emp_Rate_Per_Hr * this.Total_Emp_Work_Hrs;
-            console.log("\nTotal Employee Workinh Hours In A Month: ", this.Total_Emp_Work_Hrs);
-            console.log("total Employee Wage In One Month Is :", this.Total_Emp_Wage, "\n");
-            // return this.Total_Emp_Work_Hrs;
+            absentDay++;
+            totalworkingday++;
+            totalemphrs += emphrs;
         }
+        console.log(totalemphrs);
+        this.totalempwage = totalemphrs * this.empRatePerHrs;
+        console.log("company Name :", this.companyName, "And total Employee Wage In One Month Is :", this.totalempwage, "\n");
     };
-    return EmployeeWage;
+    return EmpWageBuilder;
 }());
-var employeeWage = new EmployeeWage();
-employeeWage.empDailyCheck();
+var JioMart = new EmpWageBuilder("JioMart", 10, 28, 100);
+JioMart.Function_EmpWage();
+console.log("\n.........................................................................................\n");
+var DMart = new EmpWageBuilder("DMart", 30, 30, 105);
+DMart.Function_EmpWage();
+console.log("\n.........................................................................................\n");
+var AbcMart = new EmpWageBuilder("AbcMart", 20, 31, 98);
+AbcMart.Function_EmpWage();
--- employeeWageCalculator.ts
+++ employeeWageCalculator.ts
@@ -1,56 +1,61 @@
 console.log("\nWelcome to Employee Wage Calculator \n");
-class EmployeeWage {
-     Emp_Rate_Per_Hr: number = 20;
-     Working_Days_Per_Month: number = 20;
-     Emp_Work_Hrs: number = 0;
-     Total_Emp_Work_Hrs: number = 0;
-     Total_Emp_Wage: number = 0;
-     Emp_Working_Days: number = 0;
-     Emp_Absent_Days: number = 0;
+class EmpWageBuilder {
+
+    companyName: string;
+    empRatePerHrs: number;
+    workingDays: number;
+    workingHrs: number;
+    totalempwage: number;
 
 
-    empDailyCheck(): void {
-        while (this.Total_Emp_Work_Hrs < 100 && this.Emp_Working_Days < 20) {
+    constructor(companyName: string, emp_ratePerHrs: number, workingDays: number, workingHrs: number) {
+        this.companyName = companyName;
+        this.empRatePerHrs = emp_ratePerHrs;
+        this.workingDays = workingDays;
+        this.workingHrs = workingHrs;
+    }
 
-            var empAttendanceCheck = Math.floor(Math.random() * 10) % 3;
+    Function_EmpWage(): void {
+        var emphrs: number = 0;
+        var totalemphrs: number = 0;
+        var totalworkingday: number = 0;
+        var absentDay: number = 0;
 
-            switch (empAttendanceCheck) {
-                case 0: {
-                    this.Emp_Absent_Days++;
-                    console.log("Employee Is Absent");
-                    this.Emp_Work_Hrs = 0;
+        while (totalemphrs <= this.workingHrs || totalworkingday < this.workingDays) {
+            
+            let CheckEmp = Math.floor(Math.random() * 10 % 3);
+
+            switch (CheckEmp) {
+                case 1:
+                    emphrs = 8;
+                    console.log("Employee is full-time present----Days are : " + totalworkingday
+                        + " & Employee Hrs is : " + emphrs);
                     break;
-                }
-                case 1: {
-
-                    console.log("Employee Is Present For FULL Day");
-                    this.Emp_Work_Hrs = 8;
+                case 2:
+                    emphrs = 4;
+                    console.log("Employee is part-time present----Days are : " + totalworkingday
+                        + " & Employee Hrs is : " + emphrs);
                     break;
-                }
-                case 2: {
-                    console.log("Employee Is Present For HALF Day");
-                    this.Emp_Work_Hrs = 4;
+                default:
+                    
+                    console.log("Employee is absent----Day are : " + absentDay);
+                    emphrs = 0;
                     break;
-                }
-                default: {
-                    console.log("Employee Not Found");
-                    break;
-                };
             }
-            this.Emp_Working_Days++;
-
-            this.Total_Emp_Work_Hrs += this.Emp_Work_Hrs;
-            console.log("Employee Working Days Are: ", this.Emp_Working_Days);
-            console.log("Employee Absent Days Are: ", this.Emp_Absent_Days, "\n");
-            this.Total_Emp_Wage = this.Emp_Rate_Per_Hr * this.Total_Emp_Work_Hrs;
-            console.log("\nTotal Employee Workinh Hours In A Month: ", this.Total_Emp_Work_Hrs);
-            console.log("total Employee Wage In One Month Is :", this.Total_Emp_Wage, "\n");
-            // return this.Total_Emp_Work_Hrs;
-
+            absentDay++;
+            totalworkingday++;
+            totalemphrs += emphrs;
         }
-    }    
+        this.totalempwage = totalemphrs * this.empRatePerHrs;
+        console.log("company Name :", this.companyName, "And total Employee Wage In One Month Is :", this.totalempwage, "\n");
+    }
 }
-var employeeWage = new EmployeeWage();
-employeeWage.empDailyCheck();
 
-    
+var JioMart = new EmpWageBuilder("JioMart", 10, 28, 100);
+JioMart.Function_EmpWage();
+console.log("\n.........................................................................................\n")
+var DMart = new EmpWageBuilder("DMart", 30, 30, 105);
+DMart.Function_EmpWage();
+console.log("\n.........................................................................................\n")
+var AbcMart = new EmpWageBuilder("AbcMart", 20, 31, 98);
+AbcMart.Function_EmpWage();
